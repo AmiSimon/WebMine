bb5bff7a011cb8fc3dd0ad5635f1ec9a6e5fa336
Skyfield1888 <skycraft811@gmail.com>
2026-02-06 17:35:15 +0100

    changed loging for fmt

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Skyfield1888/WebMine/backend"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -20,6 +21,6 @@ func main() {
 
 	// main website handeler
 	http.Handle("/", http.FileServer(http.Dir("frontend/static")))
-	log.Println("Server listening on :8082")
-	log.Fatal(http.ListenAndServe(":8082", nil))
+	fmt.Println("Server listening on :8082")
+	fmt.Fatal(http.ListenAndServe(":8082", nil))
 }
