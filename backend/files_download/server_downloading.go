ff7d827d08bda49ded196934c997b5fa480a32f9
Simon Leneveu <simon.leneveu@gmail.com>
2026-02-12 19:18:07 +0100

    Vanilla server downloading functionnality complete

--- backend/files_download/server_downloading.go
+++ backend/files_download/server_downloading.go
@@ -1,9 +1,15 @@
 package filesdownload
 
 import (
+	"Skyfield1888/WebMine/backend"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const DEFAULT_VERSION_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
@@ -22,21 +28,37 @@ const (
 	// Above to be implemented
 )
 
-type MojangVersionManifest struct {
+type MojangVersionsManifest struct {
 	LatestVersions struct {
 		Release string
 		Snapshot string
-	}
+	} `json:"latest"`
 
 	Versions []struct{
 		Id string
+		Url string
 		VersionType string `json:"type"`
-		ReleaseTime string
+		ReleaseTime time.Time
 		Sha1 string
+	} `json:"versions"`
+}
+
+type MojangVersionManifest struct {
+	Downloads struct {
+		Server struct {
+			Url string
+		}
+	}
+
+	Id string
+
+	JavaVersion struct {
+		Component string
+		MajorVersion int
 	}
 }
 
-func (manifest MojangVersionManifest) Populate(url string) error {
+func (manifest *MojangVersionsManifest) Populate(url string) error {
 	manifestData, err := http.Get(url)
 	if err != nil { return err }
 	
@@ -45,12 +67,71 @@ func (manifest MojangVersionManifest) Populate(url string) error {
 	manifestBody, err := io.ReadAll(manifestData.Body)
 	if err != nil { return err }
 	
+	// log.Print(string(manifestBody))
+
 	err = json.Unmarshal(manifestBody, &manifest)
 	if err != nil { return err }
 	
 	return nil
 }
 
-func DownloadServer(path string, serverType ServerType, version string) error {
+func GetVersionUrl(version string, manifest MojangVersionsManifest) (string, error) {
+	log.Print(manifest.Versions)
+	for _, minecraftVersion := range manifest.Versions {
+		if minecraftVersion.Id == version {
+			return minecraftVersion.Url, nil
+		}
+	}
+	return "", fmt.Errorf("Version %s is nowhere to be found.", version)
+}
+
+func GetVersionInfo(url string) (MojangVersionManifest, error) {
+	result := MojangVersionManifest{}
+	versionJson, err := http.Get(url)
+	if err != nil { return MojangVersionManifest{}, err }
+
+	defer versionJson.Body.Close()
+
+	versionData, err := io.ReadAll(versionJson.Body)
+	err = json.Unmarshal(versionData, &result)
+	if err != nil { return MojangVersionManifest{}, err }
+
+	return result, nil
+}
+
+func DownloadVanillaServer(path string, version string) error {
+	manifest := MojangVersionsManifest{}
+	err := manifest.Populate(DEFAULT_VERSION_MANIFEST_URL)
+	if err != nil { return err }
+
+	versionUrl, err := GetVersionUrl(version, manifest)
+	if err != nil { return err }
+
+	versionInfo, err := GetVersionInfo(versionUrl)
+	if err != nil {return err}
+
+	serverUrl := versionInfo.Downloads.Server.Url
+
+	out, err := os.Create(fmt.Sprintf("%sserver.jar", path))
+	if err != nil {return err}
+
+	defer out.Close()
+
+	response, err := http.Get(serverUrl)
+	if err != nil {return err}
+
+	defer response.Body.Close()
+
+	_, err = io.Copy(out, response.Body)
+	if err != nil {return err}
+
 	return nil
+}
+
+func CheckFolderStructure() error {
+	_, err := os.Stat(backend.SavedAppConfig.MinecraftServerConfig.PathToMcServers)
+	if errors.Is(err, os.ErrNotExist) {
+		return os.Mkdir(backend.SavedAppConfig.MinecraftServerConfig.PathToMcServers, 0755)
+	}
+	return err
 }
